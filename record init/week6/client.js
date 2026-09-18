cda7751a8d47faf5f70cfdf61c0c3bcd5f3ca702
shaikarbaazmehamood <shaikarbaazmehamood.24.csm@anits.edu.in>
2026-09-18 23:07:30 +0530

    Delete record init directory

--- record init/week6/client.js
+++
@@ -1,8 +0,0 @@
-const net=require("net");
-const client= net.createConnection({port:3000},()=>{
-console.log("Connected to server");
-});
-client.on("data",(data)=>{
-console.log("Server : ",data.toString());
-client.end();
-});
--- record init/week6/path.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-let filePath = "C:\\Users\\Lasya\\Documents\\test.txt";
-
-console.log("Directory:", path.dirname(filePath));
-console.log("File Name:", path.basename(filePath));
-console.log("Extension:", path.extname(filePath));
-console.log("Normalized Path:", path.normalize(filePath));
